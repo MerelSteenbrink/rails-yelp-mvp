92d99f40eb88a58022bb34e0f9d51b5ff5527ba6
Merel <merel.steenbrink@gmail.com>
2016-06-01 16:54:00 +0200

    not finished, buit basics working

---
+++ app/controllers/reviews_controller.rb
@@ -0,0 +1,24 @@
+class ReviewsController < ApplicationController
+
+  def new
+    @restaurant = Restaurant.find(params[:restaurant_id])
+    @review = Review.new
+  end
+
+  def create
+    @review = Review.create(review_params)
+    @restaurant = Restaurant.find(params[:restaurant_id])
+    @restaurant.reviews << @review
+    @restaurant.save!
+    redirect_to restaurant_path(@restaurant)
+  end
+
+
+
+  private
+
+  def review_params
+    params.require(:review).permit(:content, :rating)
+  end
+end
+
--- db/seeds.rb
+++ db/seeds.rb
@@ -5,3 +5,11 @@
 #
 #   cities = City.create([{ name: 'Chicago' }, { name: 'Copenhagen' }])
 #   Mayor.create(name: 'Emanuel', city: cities.first)
+
+
+Restaurant.create(name: 'Zeezicht', address: 'Urk', phone_number: 3456356, category: 'belgian')
+Restaurant.create(name: 'passionfruit', address: 'Guantanamastr.', phone_number: 3443456, category: 'chinese')
+Restaurant.create(name: 'Sneazy slilicon silince', address: 'end of the world', phone_number: 3563456, category: 'Japanese')
+Restaurant.create(name: 'Quatro Mori', address: 'end of the world', phone_number: 333456, category: 'Japanese')
+Restaurant.create(name: 'On the spot', address: 'craycray', phone_number: 334556, category: 'french')
+Restaurant.create(name: 'Ya no puedo mas..', address: 'rodriguez fernado', phone_number: 354566, category: 'french')
